03dafbd5584fb1e87aeae4f34d77dd0056d440e8
Teffen Ellis (Eric) <teffen@nirri.us>
2017-03-28 14:44:37 -0700

    Update example.

--- source/app.js
+++ source/app.js
@@ -1,5 +1,5 @@
 (function () {
-  "use strict"
+  'use strict'
 
   if (!window.addEventListener) return // Check for IE9+
 
@@ -8,25 +8,23 @@
 
   // updateElement runs every time the options are updated.
   // Most of your code will end up inside this function.
+  function updateElement () {
+    element = INSTALL.createElement(options.location, element)
 
-  function updateElement() {
-    element = Eager.createElement(options.location, element)
-
-    element.className = "eager-message"
-    element.textContent = options.message
+    element.setAttribute('app-id', 'example')
+    element.innerHTML = options.message
   }
 
   // This code ensures that the app doesn't run before the page is loaded.
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateElement)
-  }
-  else {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', updateElement)
+  } else {
     updateElement()
   }
 
   // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
   window.INSTALL_SCOPE = {
-    setOptions: function setOptions(nextOptions) {
+    setOptions (nextOptions) {
       options = nextOptions
 
       updateElement()
